aa196988d5d6799812f30d5200d44df2e613db2c
Mateo RODRIGUEZ <mrodrigu@e1r5p16.42.fr>
2018-04-25 07:50:22 +0200

    guille

--- srcs/create_image.c
+++ srcs/create_image.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 22:10:45 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/04/25 06:00:57 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/04/25 07:42:56 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,15 @@ void	create_image(t_win *win)
 	int		bit_per_pixel;
 	int		size_line;
 	int		endian;
+	int		width = 1920;
+	int		height = 1080;
 
 	bit_per_pixel = sizeof(int) * 8;
 	size_line = W_WIDTH * sizeof(int);
 	endian = 0;
-	if (!(win->img_ptr = mlx_new_image(win->mlx_id, W_WIDTH, W_HEIGHT)))
+//	if (!(win->img_ptr = mlx_new_image(win->mlx_id, W_WIDTH, W_HEIGHT)))
+//		exit_failure("Can not create image");
+	if (!(win->img_ptr = mlx_xpm_file_to_image(win->mlx_id, "guille.xpm", &width, &height)))
 		exit_failure("Can not create image");
 	if (!(win->img =
 		mlx_get_data_addr(win->img_ptr, &bit_per_pixel, &size_line, &endian)))
